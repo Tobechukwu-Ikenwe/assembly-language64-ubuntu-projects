ff26d3a945de785ebaa565c4da158ddce8955a06
Tobechukwu Ikenwe Othniel <tobechukwu.ikenwe@usm.edu>
2026-07-04 16:48:10 -0400

    Add assembly code for arithmetic operations
    This file contains data and text sections for basic arithmetic operations including addition, subtraction, multiplication, division, and modulus. It initializes input and output variables for these operations.

---
+++ chapter 7/7.9.2.5.s
@@ -0,0 +1,67 @@
+section .data
+
+; Input variables (unsigned)
+
+dNum1  dd  150
+dNum2  dd  10
+dNum3  dd  15
+dNum4  dd  3
+
+qNum1  dq  1500
+
+; Output variables initialized to 0
+
+dAns1   dd  0
+dAns2   dd  0
+dAns3   dd  0
+
+dAns6   dd  0
+dAns7   dd  0
+dAns8   dd  0
+
+qAns11  dq  0
+qAns12  dq  0
+qAns13  dq  0
+
+dAns16  dd  0
+dAns17  dd  0
+dAns18  dd  0
+
+dRem18  dd  0
+
+
+section .text
+
+global _start
+
+_start:
+
+;dAns1 = dNum1 + dNum2  
+mov eax, [dNum1]
+add eax, [dNum2]
+mov [dAns1], eax
+
+
+;dAns6 = dNum1 – dNum2  
+mov eax, [dNum1]
+sub eax, [dNum2]
+mov [dAns6], eax
+
+;qAns11 = dNum1 * dNum3  
+mov eax, [dNum1]
+mul dword [dNum3] 
+mov [qAns11], eax   ;lower 32
+mov [qAns11 + 4], edx  ;upper 32 
+
+
+;10.  dAns16 = dNum1 / dNum2  
+mov eax, [dNum1]
+xor edx, edx
+div dword [dNum2]
+mov [dAns16], eax
+
+;13.  dRem18 = qNum1 % dNum4  
+mov eax, [qNum1]   ; lower 32
+mov edx, [qNum1 + 4] ; upper 32
+div dword [dNum4]
+mov [dRem18], edx
